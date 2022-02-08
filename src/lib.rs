6b05ab0f4ef39da594c70688a3477969ef90033f
lemon-mint <ice.lemons.mint@gmail.com>
2022-02-08 10:31:42 +0900

    impl IsTrue for bool

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,40 @@
+//! IsTrue
+//! 
+//! A simple library for checking if a value is true.
+
+pub trait IsTrue {
+    fn is_true(&self) -> bool;
+}
+
+impl IsTrue for bool {
+    /// Check if the boolean is true
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// extern crate 
+    /// ;
+    /// use is_true_rs::IsTrue;
+    /// 
+    /// let x = true;
+    /// assert_eq!(x.is_true(), true);
+    /// ```
+    fn is_true(&self) -> bool {
+        *self == true
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::IsTrue;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn is_true_eq_true() {
+        assert_eq!(true.is_true(), true);
+    }
+
+    #[test]
+    fn is_true_eq_false() {
+        assert_eq!(false.is_true(), false);
     }
 }
